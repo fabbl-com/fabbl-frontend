3a53b4a48f8738d30fe17990f7799278aa7d868a
nandan-shah <nandan2583@gmail.com>
2022-03-01 09:15:11 +0530

    404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,45 @@
-import React from "react";
-
+import React, { useEffect, useRef } from "react";
+import lottie from "lottie-web";
+import animationData from "../assets/animation/404.json";
 const NotFound = () => {
+  const container = useRef(null);
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData
+    });
+    return () => anim.destroy();
+  }, []);
   return (
-    <div>
-      <h1>jhdasjdhasjdhajshdajkdhsadsbbcxbdahsdhasdjhadjhsadnn</h1>
-      <h1>jhdasjdhasjdhajshdajkdhsadsbbcxbdahsdhasdjhadjhsadnn</h1>
-      <h1>jhdasjdhasjdhajshdajkdhsadsbbcxbdahsdhasdjhadjhsadnn</h1>
-      <h1>jhdasjdhasjdhajshdajkdhsadsbbcxbdahsdhasdjhadjhsadnn</h1>
-    </div>
+    <>
+      <div
+        style={{
+          backgroundColor: " black",
+          height: "100vh",
+          position: "fixed",
+          overflow: "hidden"
+        }}
+        ref={container}></div>
+      <div
+        style={{
+          color: "white",
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+          textAlign: "center",
+          marginTop: "20%",
+          scale: "13"
+        }}>
+        <center>
+          <h2>404</h2>
+          <br />
+          <h1>Page Not Found</h1>
+        </center>
+      </div>
+    </>
   );
 };
 
