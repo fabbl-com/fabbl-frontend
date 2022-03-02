835b8333dd4b85caf5dc3312d8fdf57e6718c6f6
nandan-shah <nandan2583@gmail.com>
2022-03-02 15:58:42 +0530

    feature: PWA

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,47 +1,7 @@
 import React, { useEffect, useRef } from "react";
-import lottie from "lottie-web";
-import animationData from "../assets/animation/404.json";
+
 const NotFound = () => {
-  const container = useRef(null);
-  useEffect(() => {
-    const anim = lottie.loadAnimation({
-      container: container.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData
-    });
-    return () => anim.destroy();
-  }, []);
-  return (
-    <>
-      <div
-        style={{
-          backgroundColor: " black",
-          height: "100vh",
-          position: "fixed",
-          overflow: "hidden"
-        }}
-        ref={container}></div>
-      <div
-        style={{
-          fontSize: "2rem",
-          color: "white",
-          position: "fixed",
-          width: "100%",
-          height: "100%",
-          textAlign: "center",
-          marginTop: "20%",
-          scale: "13"
-        }}>
-        <center>
-          <h2>404</h2>
-          <br />
-          <h1 style={{ marginTop: "1ch" }}>Page Not Found</h1>
-        </center>
-      </div>
-    </>
-  );
+  return <div>Not found</div>;
 };
 
 export default NotFound;
