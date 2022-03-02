5616374beba0e27ef636e4af6ec44b64b33fdff1
nandan-shah <nandan2583@gmail.com>
2022-03-02 21:48:37 +0530

    404 page updated

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,39 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import notFoundSvg from "../assets/animation/404.svg";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundImage: `url(${notFoundSvg})`,
+    height: "100vh",
+    backgroundPosition: " center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "80vw 90vh",
+    [theme.breakpoints.down("md")]: {
+      backgroundPosition: " top"
+    }
+  },
+  button: {
+    marginTop: "40vh"
+  }
+}));
 
 const NotFound = () => {
-  return <div>Not found</div>;
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.root}>
+        {" "}
+        <center>
+          <Button className={classes.button} variant="outlined" color="primary" href={"/"}>
+            {" "}
+            Go Home
+          </Button>
+        </center>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
